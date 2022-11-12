b467fe169fa0b794615d749ce0fe7cac22849614
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-11-12 14:55:49 +0000

    .

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+  Scanner sc1 = new Scanner(System.in);
+    String stringUser, stringUser2;
+
+  System.out.println("Escreva uma frase: ");
+    stringUser = sc1.nextLine();
+  System.out.println("Escreva uma outra frase: ");
+    stringUser2 = sc1.nextLine();
+    if(stringUser == "" || stringUser2 ==""){
+      System.out.println("Favor inserir uma frase em ambas variáveis!");
+    }
+  System.out.println("Sua segunda frase: " + stringUser2 + " contém algo da primeira?: " + stringUser );
+        if(stringUser.contains(stringUser2)) {
+            System.out.println("Contém.");
+        }
+        else {
+            System.out.println("Não contém.");
+        }
   }
 }
